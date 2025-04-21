04bae37dae852cacea415e63a4bc9768e72cdb55
Anrylis <kitty532286@gmail.com>
2025-04-21 20:18:15 +0800

    first commit annoyed by PI

---
+++ src/Cylinder.cpp
@@ -0,0 +1,39 @@
+# ifndef CYLINDER_CPP
+# define CYLINDER_CPP
+# include "Cylinder.h"
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+double Cylinder :: SurfaceArea(){
+    
+    return     
+        2*radius*radius*M_PI + 2*M_PI*radius*height;
+}
+
+double Cylinder :: Volume(){
+
+    return M_PI*radius*radius*height;
+}
+
+double Cylinder :: Circumference(){
+
+    return 2*M_PI*radius;
+}
+
+istream & operator>>(istream & in, Cylinder & cldr)
+{
+    in >>  cldr.radius >> cldr.height; 
+    return in;
+}
+
+ostream & operator<<(ostream & out, Cylinder & cldr)
+{
+    cout << fixed<<std::setprecision(3) << "Circumference: " << cldr.Circumference() << endl;
+    cout << fixed<<std::setprecision(3) << "SurfaceArea: "   <<  cldr.SurfaceArea() << endl;
+    cout << fixed<<std::setprecision(3) << "Volume: "        <<  cldr.Volume() << endl;
+    return out;
+}
+
+# endif
